51309eefda8838203af593b39d43424223b26e54
MSM_707 <sirojiddinov.1881@mail.ru>
2018-08-18 22:07:31 +0500

    Setting up sockets

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require('express'); // express  library
+const path = require('path'); // libaray to create relative path of file
+const http = require('http'); //http library to create server
+const socketIO = require('socket.io');
+
+var port = process.env.PORT || 3000;
+var publicPath = path.join(__dirname, '../public'); // creates relative path to public folder
+var app = express();
+app.use(express.static(publicPath)); // renders index.html file to browser
+var server = http.createServer(app);
+var io = socketIO(server);
+io.on("connection", (socket) => {
+	console.log('New user connected');
+
+	socket.on('disconnect', () => {
+		console.log('User was disconnected');
+	});
+});
+
+
+server.listen(port, () => {
+	console.log(`Server is running on port: ${port}`);
+});
